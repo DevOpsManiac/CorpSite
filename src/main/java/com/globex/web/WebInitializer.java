37c4b98f89e6bc7597c0a77ea5258fc36328bedd
Colin O\'Brien <colin.obrien@servicenow.com>
2019-02-26 22:59:15 -0500

    COR-4: Needed a commit

--- src/main/java/com/globex/web/WebInitializer.java
+++ src/main/java/com/globex/web/WebInitializer.java
@@ -5,6 +5,7 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
+    // Need to force a commit
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return null;
